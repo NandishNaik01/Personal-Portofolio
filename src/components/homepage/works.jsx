3e55c21bfcae3731eff779efaa422550172f073f
Nandish D Naik <naik.nandishd@gmail.com>
2024-05-17 03:22:38 +0530

    added Great Learning Certificates

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -14,17 +14,49 @@ const Works = () => {
 				body={
 					<div className="works-body">
 						<div className="work">
-							
-							<ul>								
-								<h4><li><a href="/Pal0Alt0Certificate.pdf">Cyber Security by <strong>PalOAltO</strong></a></li></h4>
-								<h4><li><a href="/BigdataCertificate.pdf">Big Data By Ekathva Innovations P.Ltd</a></li></h4>
-								<h4><li><a href="/jscert.png">JavaScript Certification test</a></li></h4>
+							<ul>
+								<h4>
+									<li>
+										<a href="/Pal0Alt0Certificate.pdf">
+											Cyber Security by{" "}
+											<strong>PalOAltO</strong>
+										</a>
+									</li>
+								</h4>
+								<h4>
+									<li>
+										<a href="/BigdataCertificate.pdf">
+											Big Data By Ekathva Innovations
+											P.Ltd
+										</a>
+									</li>
+								</h4>
+								<h4>
+									<li>
+										<a href="/jscert.png">
+											JavaScript Certification test
+										</a>
+									</li>
+								</h4>
+								<h4>
+									<li>
+										<a href="/android.png">
+											Android Development By Great
+											Learning
+										</a>
+									</li>
+								</h4>
+								<h4>
+									<li>
+										<a href="/react.png">
+											React.js By Great Learning
+										</a>
+									</li>
+								</h4>
 								{/* <h4><li><a href="/res.pdf">Mern Stack by MICROFOST</a></li></h4>
 								<h4><li><a href="/res.pdf">UX/UI BY GOOGLE</a></li></h4> */}
 							</ul>
 						</div>
-
-						
 					</div>
 				}
 			/>
